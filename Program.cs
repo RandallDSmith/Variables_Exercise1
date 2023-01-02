66a11c4776cea04acbe4ffbaff7849712ba27c63
Randall Smith <rdsmith1761@gmail.com>
2023-01-02 13:55:17 -0500

    Variables Exercise 1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Declare and initialize variables
+using System.Runtime.CompilerServices;
+
+string name = "John";
+int age = 31;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 79.9;
+decimal currentPrice = 29.99m;
+
+Console.WriteLine($"Name:  {name}");
+Console.WriteLine($"Age:  {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18:  {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
